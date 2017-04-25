53439b74f0d1a8951c4ac9db8295dee597045c61
Scott Alzheimer <salzheimer@gmail.com>
2017-04-24 20:54:32 -0700

    Initial Commit

---
+++ CareOregonTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CareOregonTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+        }
+
+        public int CalculateFibonacci(int position)
+        {
+            int first = 1;
+            int second = 1;
+
+            for (int i = 0; i < position; i++)
+            {
+                int temp = first;
+                first = second;
+                second = temp + second;
+                Console.WriteLine("position " + i + ": value " + second);
+            }
+            return first;
+        }
+    }
+}
---
+++ CareOregonTest_Tests/Program_Tests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CareOregonTest; 
+namespace CareOregonTest_Tests
+{
+    [TestClass]
+    public class Program_Tests
+    {
+        [TestMethod]
+        public void CalculateFibonacci_CanOuput()
+        {
+            var co =  new CareOregonTest.Program() ;
+            var result = co.CalculateFibonacci(3);
+
+            Assert.IsTrue(result > 0, "Result not greater than zero");
+
+        }
+        [TestMethod]
+        public void CalculateFibonacci_CanOuput_Position_10()
+        {
+            var co = new CareOregonTest.Program();
+            var result = co.CalculateFibonacci(10);
+
+            Assert.IsTrue(result > 0, "Result not greater than zero");
+            Assert.AreEqual(89, result, "Result for position 10 is " + result + " and should be 89");
+        }
+        [TestMethod]
+        public void CalculateFibonacci_CanOuput_Positions_40()
+        {
+            var co = new CareOregonTest.Program();
+            var result = co.CalculateFibonacci(40);
+
+            Assert.IsTrue(result > 0, "Result not greater than zero");
+            Assert.AreEqual(165580141, result, "Result for position 40 is " + result + " and should be 165580141");
+        }
+    }
+}
